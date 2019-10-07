1f4e0910cd58e32a167691d0277df5b53084a420
Jiaan Zeng <l.allen09@gmail.com>
2019-10-06 21:03:23 -0700

    add plotting

--- chapter02/my_ten_armed_testbed.py
+++ chapter02/my_ten_armed_testbed.py
@@ -7,6 +7,7 @@ class BanditEnv:
 
     def __init__(self, actions):
         self.q_star = [np.random.randn() for i in range(actions)]
+        self.best_action = np.argmax(self.q_star)
 
     def get_reward(self, action):
         return self.q_star[action] + np.random.randn()
@@ -40,9 +41,10 @@ class Config:
 
 def simulate(config):
     rewards = np.zeros((config.times, config.steps))
+    best_action_count = np.zeros((config.times, config.steps))
 
     for t in range(config.times):
-        agent = BasicAgent(alpha=0.1, epsilon=0.05, actions=10)
+        agent = BasicAgent(alpha=0.1, epsilon=config.epsilon, actions=10)
         environment = BanditEnv(actions=10)
 
         for s in range(config.steps):
@@ -51,13 +53,33 @@ def simulate(config):
             agent.update(action, reward)
 
             rewards[t, s] = reward
+            best_action_count[t, s] = 1 if action == environment.best_action else 0
 
-    return rewards.mean(axis=0)
+    return best_action_count.mean(axis=0), rewards.mean(axis=0)
+
+
+def figure_2_2():
+    epsilons = [0, 0.01, 0.1]
+    results = []
+
+    for epsilon_val in epsilons:
+        configuration = Config(alpha=0.1, epsilon=epsilon_val, times=500, steps=2000)
+        mean_best_action_count, mean_reward = simulate(configuration)
+        results.append((epsilon_val, mean_best_action_count, mean_reward))
+
+    plt.figure(figsize=(10, 20))
+    plt.subplot(2, 1, 1)
+    for item in results:
+        plt.plot(range(len(item[2])), item[2], label='epsilon = %.02f' % item[0])
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    for item in results:
+        plt.plot(range(len(item[1])), item[1], label='epsilon = %.02f' % item[0])
+    plt.legend()
+
+    plt.show()
 
 
 if __name__ == '__main__':
-    configuration = Config(alpha=0.1, epsilon=0.05, times=100, steps=2000)
-
-    mean_reward = simulate(configuration)
-    plt.plot(range(len(mean_reward)), mean_reward)
-    plt.show()
+    figure_2_2()
